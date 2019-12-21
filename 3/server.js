f935b7198fa4c8da84957ce66723a9a599c7f5cf
michaelef123 <s0545163@htw-berlin.de>
2019-12-21 08:25:39 +0100

    removed changes from exercise 3 on backup master

--- 3/server.js
+++
@@ -1,5 +0,0 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./typedefs.js');
-const resolvers = require('./resolvers.js')
-
-module.exports = new ApolloServer({ typeDefs, resolvers });
